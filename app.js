b2c5bb3cce4e2d09a362bc13e86ef9aeb259bcde
Vasil Bit <vasil.bituni@gmail.com>
2021-03-03 10:58:35 +0100

    Add  movies to array

--- app.js
+++ app.js
@@ -8,5 +8,28 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary() {
+  const title = document.getElementById('book-title').value;
+  const author = document.getElementById('book-author').value;
+  const pages = document.getElementById('book-pages').value;
+  const read = document.getElementById('book-read').checked;
+
+let newBook
+ 
+function addBook() {
+  newBook = new Book(title, author, pages, read)  
+  console.log(newBook);
+  return newBook
+}
+
+addBook()
+
+
+myLibrary.push(newBook)
+console.log(myLibrary);
+
 
 }
+
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', addBookToLibrary)
